c5b79de1dbdbbe8574fa46d85fbc554c4765a0d5
Thilo Richter <thilo@Thilos-MacBook-Pro.local>
2016-03-16 22:02:59 +0100

    concat and minify CSs

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var concatCss = require('gulp-concat-css');
+var minifyCss = require('gulp-minify-css');
 
 gulp.task('concatScripts', function(){
   //array of filenames or string.
@@ -18,5 +20,22 @@ gulp.task('minifyScripts', function(){
   // you can use gulp-rename for renaming the files
   //.pipe(rename['app.min.js'])
   .pipe(gulp.dest('assets/js'));
+});
+
+gulp.task('concatCss', function(){
+  //array of filenames or string.
+  // If the load order matters make sure it's in the right order
+  gulp.src(['assets/css/bootstrap.min.css', 'assets/css/font-awesome.min.css', 'assets/css/style.css']) //Takes readable stream of data and concats it
+  .pipe(concatCss("app.css")) // Takes a readable stream of data and writes it to disk
+  .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('minifyCss', function(){
+  gulp.src('assets/css/app.css')
+  .pipe(minifyCss())
+  // This overwrites the original file though. Take care. You should rename the file as you minify it
+  // you can use gulp-rename for renaming the files
+  //.pipe(rename['app.min.js'])
+  .pipe(gulp.dest('assets/css'));
 });
 
